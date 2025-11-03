e0f41ea485cf737adc0eda5a8251ba91560c2656
MyMainPC <chajihun1171@gmail.com>
2025-11-03 22:48:16 +0900

    bird 속도, 날개짓 완성

--- bird.py
+++ bird.py
@@ -2,6 +2,18 @@ from pico2d import *
 import game_world
 import game_framework
 
+# Bird Fly Speed
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+FLY_SPEED_KMPH = 54.0  # Km / Hour
+FLY_SPEED_MPM = (FLY_SPEED_KMPH * 1000.0 / 60.0)
+FLY_SPEED_MPS = (FLY_SPEED_MPM / 60.0)
+FLY_SPEED_PPS = (FLY_SPEED_MPS * PIXEL_PER_METER)
+
+# Bird Action Speed
+TIME_PER_ACTION = 0.5 # 액션 당 걸리는 시간
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION # 초당 액션 수
+FRAMES_PER_ACTION = 14
+
 class Bird:
     def __init__(self, x = 400 , y = 500):
         self.image = load_image('bird_animation.png')
@@ -11,14 +23,14 @@ class Bird:
         self.flip = ''
 
     def draw(self):
-        self.image.clip_composite_draw((self.frame % 5) * 183, (2 - (self.frame // 5)) * 168, 180, 168, 0, self.flip, self.x, self.y, 100, 100)
+        self.image.clip_composite_draw(int(self.frame % 5) * 183, (2 - (int(self.frame) // 5)) * 168, 180, 168, 0, self.flip, self.x, self.y, 70, 35)
 
     def update(self):
-        self.frame = (self.frame + 1) % 14
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 14
+        self.x += self.dir * FLY_SPEED_PPS * game_framework.frame_time
         if self.x > 1600:
             self.dir = -1
             self.flip = 'h'
         elif self.x < 0:
             self.dir = 1
             self.flip = ''
-        delay(0.1)
